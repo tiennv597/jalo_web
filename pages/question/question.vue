35b053020e0e2ffbb10d88298fe1465e66095d07
tiennv <tiennv597@gmail.com>
2020-08-05 12:01:59 +0700

    setup project

--- pages/question/question.vue
+++ pages/question/question.vue
@@ -3,6 +3,46 @@
     <v-container sm12>
       <v-layout row wrap>
         <v-flex sm11 id="question">
+          <!--  -->
+          <v-card>
+            <v-layout row>
+              <v-flex xs1></v-flex>
+              <v-flex xs1>
+                <v-select
+                  label="Cấp độ (N5~N1)"
+                  :items="levels"
+                  v-model="level"
+                  item-text="country"
+                  item-value="abbr"
+                ></v-select>
+              </v-flex>
+              <v-flex xs1></v-flex>
+              <v-flex xs1>
+                <v-select
+                  v-model="type"
+                  :hint="`${type.name}, ${type.id}`"
+                  :items="types"
+                  item-text="name"
+                  item-value="id"
+                  label="Select"
+                  persistent-hint
+                  return-object
+                  single-line
+                  :click="onChangeType()"
+                ></v-select>
+              </v-flex>
+              <v-flex xs1></v-flex>
+              <!-- <v-flex xs1>
+                <v-select
+                  label="Types"
+                  :items="types"
+                  v-model="type"
+                  item-text="country"
+                  item-value="abbr"
+                ></v-select>
+              </v-flex>-->
+            </v-layout>
+          </v-card>
           <!-- question -->
           <v-card>
             <v-layout row>
@@ -21,38 +61,44 @@
               </v-flex>
             </v-layout>
           </v-card>
-
-          <!-- Answer option 1 -->
-
+          <!-- Answer option  -->
           <div v-for="item in items" :key="item.id">
             <!-- nội dung -->
             <v-card>
               <v-layout row>
                 <v-flex xs1>
                   <v-subheader>
-                    <v-checkbox v-model="result" color="red" value="red" hide-details></v-checkbox>
+                    <v-checkbox
+                      v-model="result"
+                      :id="item.checked"
+                      color="blue"
+                      :value="item.checked"
+                      hide-details
+                    ></v-checkbox>
                   </v-subheader>
                 </v-flex>
                 <v-flex xs10>
                   <v-text-field
                     multi-line
                     rows="1"
-                    v-model="answer1"
+                    :v-model="item.id"
                     clearable
-                    :label="item.hind"
+                    :label="item.label"
                     type="text"
                   ></v-text-field>
                 </v-flex>
               </v-layout>
             </v-card>
           </div>
+          <span>Checked names: {{ result }} {{ level }}</span>
           <v-card>
             <v-layout row>
               <v-flex xs1>
                 <v-subheader></v-subheader>
               </v-flex>
               <v-flex xs11>
-                <v-btn color="info" v-on:click="addQuestion">Add answer option</v-btn>
+                <v-btn color="error" v-on:click="deleteOption">Delete answer option</v-btn>
+                <v-btn color="info" v-on:click="addOption">Add answer option</v-btn>
                 <v-btn color="success">Save</v-btn>
               </v-flex>
             </v-layout>
@@ -71,67 +117,72 @@
   </v-form>
 </template>
 <script>
-import VWidget from "@/components/VWidget";
-import Answer from "@/components/Answer";
 import Vue from "vue";
-// Vue.component("button-counter", {
-//   template: '<button v-on:click="count++">Bạn đã bấm {{ count }} lần.</button>',
-// });
 new Vue({ el: "#question" });
 export default {
   layout: "dashboard",
-  components: {
-    VWidget,
-    Answer,
-  },
+  components: {},
   data() {
     return {
       items: [
         {
-          id: "answer 1",
-          hind: "Answer option 1",
+          id: "answer1",
+          label: "Answer option 1",
+          checked: "1",
         },
         {
-          id: "answer 2",
-          hind: "Answer option 2",
+          id: "answer2",
+          label: "Answer option 2",
+          checked: "2",
         },
         {
-          id: "answer 3",
-          hind: "Answer option 3",
+          id: "answer3",
+          label: "Answer option 3",
+          checked: "3",
         },
         {
-          id: "answer 4",
-          hind: "Answer option 4",
+          id: "answer4",
+          label: "Answer option 4",
+          checked: "4",
         },
       ],
-      message: "",
-      answer1: "",
+      levels: ["N1", "N2", "N3", "N4", "N5"],
+      level: "",
+      // type: { id: 1, name: "Từ vựng" },
+      // types: [
+      //   { id: "1", name: "Từ vựng" },
+      //   { id: "2", name: "Ngữ pháp" },
+      //   { id: "3", name: "Đọc" },
+      //   { id: "4", name: "Nghe" },
+      // ],
+      type: { id: "1", name: "Từ vựng" },
+      types: [
+        { id: "1", name: "Từ vựng" },
+        { id: "2", name: "Ngữ pháp" },
+        { id: "3", name: "Đọc" },
+        { id: "4", name: "Nghe" },
+      ],
+      result: [],
       loading: false,
-      email: "",
-      rules: {
-        required: (value) => !!value || "Required.",
-        email: (value) => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return pattern.test(value) || "Invalid e-mail.";
-        },
-      },
     };
   },
   computed: {},
   methods: {
-    addQuestion() {
+    addOption() {
       console.log("test add question");
-      this.$store.dispatch("QUESTION/addAnswer", {
+      this.$store.dispatch("QUESTION/addOption", {
         items: this.items,
       });
     },
-    clickMe() {
-      this.loading = true;
-      this.message = "Wait for it...";
-      setTimeout(() => {
-        this.loading = false;
-        this.message = "You've clicked me!";
-      }, 2000);
+    deleteOption() {
+      console.log("test add question");
+      this.$store.dispatch("QUESTION/deleteOption", {
+        items: this.items,
+      });
+    },
+    onChangeType() {
+      //console.log(type.id)
+      alert("onchange trigerred: " + this.type.id)
     },
   },
 };
--- store/modules/question/actions.js
+++ store/modules/question/actions.js
@@ -1,11 +1,18 @@
 export default {
-    addAnswer({ commit }, items) {
+    addOption({ commit }, items) {
         let index = items.items.length + 1;
         const item = {
-            id: "answer " + index,
-            hind: "Answer option " + index,
+            id: "answer" + index,
+            label: "Answer option " + index,
+            checked: index,
         }
         items.items.push(item);
+        commit("SET_ITEM", items.items);
+
+    },
+    deleteOption({ commit }, items) {
+        let index = items.items.length - 1;
+        items.items.splice(index, 1);
         commit("SET_ITEM", items.items);
 
     }
